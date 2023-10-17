04be3a7739f5c470de6e5bbe2da45df24a5ab144
panta96pranav <panta96pranav@hotmail.com>
2023-10-17 18:43:33 -0300

    test update
    Signed-off-by: panta96pranav <panta96pranav@hotmail.com>

---
+++ src/app.py
@@ -0,0 +1,93 @@
+# #!/usr/bin/env python3
+import requests
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import json
+import os
+from flask import Flask, request, flash, url_for, redirect, render_template
+from sqlalchemy import func
+
+app = Flask(__name__)
+script = os.path.realpath(__file__)
+path = os.path.dirname(script)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+path+'//currency_exchangeRates_db.sqlite3'
+
+db = SQLAlchemy(app)
+
+class currency_exchangeRates_db(db.Model):
+    currency_id = db.Column('currency_id', db.Integer, primary_key = True)
+    base_currency = db.Column(db.String(5), nullable=False)
+    GBP_currency_rate = db.Column(db.String(50), nullable=False)
+    CAD_currency_rate = db.Column(db.String(50), nullable=False)
+    JPY_currency_rate = db.Column(db.String(50), nullable=False)
+    INR_currency_rate = db.Column(db.String(50), nullable=False)
+    HKD_currency_rate = db.Column(db.String(50), nullable=False)
+    last_update_on = db.Column(db.DateTime,default="datetime.utcnow")
+
+def __init__(self, base_currency,GBP_currency_rate,CAD_currency_rate,JPY_currency_rate,INR_currency_rate,HKD_currency_rate,last_update_on):
+   self.base_currency = base_currency
+   self.GBP_currency_rate = GBP_currency_rate
+   self.CAD_currency_rate = CAD_currency_rate
+   self.JPY_currency_rate = JPY_currency_rate
+   self.INR_currency_rate = INR_currency_rate
+   self.HKD_currency_rate = HKD_currency_rate   
+   self.last_update_on = last_update_on   
+
+# '''
+# Helper function to get Currency Exchnage Rates
+# using API
+# '''
+def get_Currency_Exchnage_Rates(currency):
+    response = requests.get("https://open.er-api.com/v6/latest/"+currency)
+    return response.json()
+
+
+@app.route('/',methods = ['GET', 'POST'])
+
+def index():  
+
+ currency="USD"
+
+ if request.method == 'POST':
+
+   currency = request.form['currency']   
+
+   currentDate = datetime.now().strftime('%Y-%m-%d')
+   
+   currency_exists = currency_exchangeRates_db.query.filter(func.DATE(currency_exchangeRates_db.last_update_on)==currentDate,currency_exchangeRates_db.base_currency==currency).first()
+   
+   if currency_exists is None: 
+
+      currency_info = request.form['get_currency_info']
+      response= get_Currency_Exchnage_Rates(currency)
+      ts = int(response["time_last_update_unix"])
+      last_update_date = datetime.utcfromtimestamp(ts)  
+      CurrencyExchnageRate = currency_exchangeRates_db(
+                                                         base_currency=response['base_code'], 
+                                                         GBP_currency_rate=str(response["rates"]["GBP"]), 
+                                                         CAD_currency_rate=str(response["rates"]["CAD"]),
+                                                         JPY_currency_rate=str(response["rates"]["JPY"]),
+                                                         INR_currency_rate=str(response["rates"]["INR"]),
+                                                         HKD_currency_rate=str(response["rates"]["HKD"]),
+                                                         last_update_on=last_update_date
+                                                      )
+      db.session.add(CurrencyExchnageRate)
+      db.session.commit()
+  
+ currencyExchnage = currency_exchangeRates_db.query.filter_by(base_currency=currency).all()
+ return render_template('index.html', selected_currency=currency, currencyexchange=currencyExchnage)
+
+
+# Get the daily currency updates from USD to GBP,CAD,JPY,INR,HKD
+if __name__ == '__main__':
+   with app.app_context():
+      db.create_all()    
+   app.run(debug = True)
+
+
+
+   
+
+  
